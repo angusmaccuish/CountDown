6e52b30040ded614a75632b7d0854714f308632a
angusmaccuish <angus.maccuish@gmail.com>
2015-11-16 15:43:46 +0000

    Less code

--- CountDown.hs
+++ CountDown.hs
@@ -1,12 +1,14 @@
 module Main where
 
-import Data.List (delete, permutations)
+import Data.List (delete)
+import Data.Set (fromList, toList)
 
-data Op   = Divide | Multiply | Add | Subtract
-            deriving (Eq)
+data Operator = Divide | Multiply | Add | Subtract deriving (Eq, Enum, Ord)
+data Expr     = Literal Int | Expr Operator Expr Expr deriving (Eq, Ord)
 
-data Expr = Literal Int | Expr Op Expr Expr
-            deriving (Eq)
+class OperatorRules a where
+  isValid :: a -> Bool
+  largestFirst :: (a,a) -> (a,a)
 
 instance Show Expr where
   show (Literal x)      = show x
@@ -17,6 +19,12 @@ instance Show Expr where
                        Add      -> " + "
                        Subtract -> " - "
 
+instance OperatorRules Expr where
+  isValid (Expr Divide x y)             = eval y > 1 && rem (eval x) (eval y) == 0
+  isValid (Expr Multiply _ (Literal 1)) = False
+  isValid (Expr Subtract x y)           = x /= y
+  isValid _                             = True
+  largestFirst (x,y) = if eval x >= eval y then (x,y) else (y,x)
 
 -- Evaluate an Expression
 eval :: Expr -> Int
@@ -26,52 +34,33 @@ eval (Expr Divide x y)   = (eval x) `div` (eval y)
 eval (Expr Add x y)      = (eval x) + (eval y)
 eval (Expr Subtract x y) = (eval x) - (eval y)
 
-
--- Find solution(s) - if any
+-- Find solution(s) - get all valid combinations of literals and operators and search for target
 solve :: Int -> [Int] -> [Expr]
-solve target xs = combinations xs >>= search
+solve target xs = (toList . fromList) (combinations xs >>= search)
   where search :: Expr -> [Expr]
         search e = (if eval e == target then [e] else []) ++ case e of
           Expr _ x y -> search x ++ search y
           Literal _  -> []
 
-
 -- All combinations
 combinations :: [Int] -> [Expr]
-combinations [] = []
+combinations []      = []
 combinations numbers = map head $ combinations' (map Literal numbers)
   where combinations' :: [Expr] -> [[Expr]]
         combinations' [] = []
         combinations' (x:[]) = [[x]]
-        combinations' xs = (concat $ [ filter (not . null) $ map (reduce' op) (pairs xs) | op <- ops ]) >>= combinations'
-          where ops = [Multiply, Divide, Add, Subtract]
-                reduce' :: Op -> ((Expr,Expr), [Expr]) -> [Expr]
-                reduce' op ((x,y), remaining) = if isValid op then (Expr op x y) : remaining else []
-                  where isValid :: Op -> Bool
-                        isValid op = case op of
-                          Divide    -> y' > 1 && rem x' y' == 0
-                          Multiply  -> y' /= 1
-                          Subtract  -> x' /= y'
-                          otherwise -> True
-                        x' = eval x
-                        y' = eval y
-
-
--- For a list of Expr(essions), return every possible pair, along with the remaining Expr's
-pairs :: [Expr] -> [((Expr, Expr), [Expr])]
-pairs [] = []
-pairs (x:[]) = []
-pairs xs = pairs' xs
-  where pairs' :: [Expr] -> [((Expr, Expr), [Expr])]
-        pairs' [] = []
-        pairs' (y:ys) = [(ordered (y,z), (delete y . delete z) xs) | z <- ys] ++ pairs' ys
-          where ordered :: (Expr,Expr) -> (Expr,Expr)
-                ordered (x,y) = if (eval x) >= (eval y) then (x,y) else (y,x)
-
+        combinations' xs = (concat $ [ filter (not . null) $ map (reduce op) (pairs xs) | op <- [Divide ..] ]) >>= combinations'
+          where pairs :: [Expr] -> [((Expr, Expr), [Expr])]
+                pairs [] = []
+                pairs [x] = []
+                pairs (y:ys) = [(largestFirst (y,z), (delete y . delete z) xs) | z <- ys] ++ pairs ys
+                reduce :: Operator -> ((Expr,Expr), [Expr]) -> [Expr]
+                reduce op ((x,y), remaining) = if isValid e then e:remaining else []
+                  where e = (Expr op x y)
 
 -- Main
 main :: IO ()
 main = do let values = [25,100,10,8,3,5]
           let target = 954
-          let solution = solve target values
-          putStrLn $ show solution
+          let solutions = solve target values
+          putStrLn (show solutions)
